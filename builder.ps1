37d4ef3bb409edd5b08050e9528cce391b2b4eea
Morro Luarvik <dr.morro.l@gmail.com>
2023-06-12 16:48:13 +1000

    create archive

---
+++ builder.ps1
@@ -0,0 +1,21 @@
+# проверка .\builder.ps1 lol kek
+param ($SourcePath, $ArchiveName="file.zip", $DestPath)
+
+if ($null -eq $DestPath) { $DestPath = Get-Content Env:\TEMP }
+
+Write-Output "sourcePath: $SourcePath";
+Write-Output "name: $Name";
+Write-Output "destPath: $DestPath";
+
+$DestFileName = Join-Path $DestPath $ArchiveName
+Write-Output "DestFileName: $DestFileName"
+Compress-Archive -Path $SourcePath -DestinationPath $DestFileName
+
+#$tmp = Get-Content Env:\TEMP
+
+# Write-Output $tmp
+#function New-TemporaryDirectory {
+#    $parent = [System.IO.Path]::GetTempPath()
+#    $name = [System.IO.Path]::GetRandomFileName()
+#    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+#}
