1b75f504161cb9a94c3e3c4bb3143e5d6ae3df56
Morro Luarvik <dr.morro.l@gmail.com>
2023-07-05 15:59:27 +1000

    has function

--- executor.ps1
+++ executor.ps1
@@ -2,24 +2,41 @@ param ($SourceDir, $ProjectFolder, $ProjectFile = "project.json")
 
 . $PSScriptRoot\misc.ps1
 
+<#
+    получить список функций
+    yc sls fn list --format json 
+
+    получить функцию hello
+    yc sls fn get --name hello --format json
+#>
+
 # останавливать выполнение скрипта при ошибке
 $ErrorActionPreference = "Stop"
 
 Write-Output "Start executor"
 
 function local:PrintHandler($handler = "default") {
-    Write-Output $handler
+    Write-Output "hh: $handler"
 }
 
-$Project = Get-Content (Join-Path $ProjectFolder $ProjectFile) | ConvertFrom-Json
-$Project.functions | Get-Member -type NoteProperty | ForEach-Object { 
-    $function = $Project.functions."$($_.Name)"
+$kek = @{handler = "single"; test = 314}
+#$kek.GetType().FullName
+PrintHandler @kek
+
+
+$Project = Get-Content (Join-Path $ProjectFolder $ProjectFile) | ConvertFrom-Json # | ConvertTo-HashTable
+$Project.functions | Get-Member -type NoteProperty | ForEach-Object {  #  
+    
+    $func = $Project.functions."$($_.Name)"
+    #$func.GetType().FullName
 
     Write-Output $_.Name
-    PrintHandler @function
+    #PrintHandler $func
     ##Write-Output $Project.functions."$($_.Name)".handler
     Write-Output " ============== "
 }
+
+HasFunction hello1
 
 <#
 $dest = CreateArchive $SourceDir
