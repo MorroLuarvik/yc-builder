48785202a8f03c2006ba4b92cab18e7ec486a2fa
Morro Luarvik <dr.morro.l@gmail.com>
2023-11-18 13:09:04 +1000

    код запуска скрипта

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Сборка и публикация Python проекта на YCloud
 
 Здесь будут примеры кода
+
+Запуск скрипта `test1.ps1`
+```shell
+ ./test1.ps1 -ProjectDir kek -SourceDir lol
+```
---
+++ test1.ps1
@@ -0,0 +1,5 @@
+param ($SourceDir, $ProjectDir, $ProjectFile = "project.json")
+
+Write-Output "Test 1 script"
+
+Write-Output "SourceDir: $SourceDir, ProjectDir: $ProjectDir, ProjectFile: $ProjectFile"
